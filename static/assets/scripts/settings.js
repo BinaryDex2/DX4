8f7dd2a3666252e3b941fda47c36e79ef669676a
Dex <82985536+BinaryDex@users.noreply.github.com>
2024-04-13 19:01:43 -0400

    Update settings.js

--- static/assets/scripts/settings.js
+++ static/assets/scripts/settings.js
@@ -1,4 +1,6 @@
 window.localStorage.setItem('v4Particles', 'true')
+const switches = document.getElementById('2')
+switches.checked = true
 
 // Ads
 document.addEventListener('DOMContentLoaded', function () {
