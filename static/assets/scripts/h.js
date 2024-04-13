482fa5d20f039501335a936b68b7b8bd97a13763
Dex <82985536+BinaryDex@users.noreply.github.com>
2024-04-13 18:47:37 -0400

    icon

--- static/assets/scripts/h.js
+++ static/assets/scripts/h.js
@@ -164,16 +164,18 @@ document.addEventListener('DOMContentLoaded', function (event) {
 })
 
 let splashtext = [
-  'Over 8 Million Users since 2023',
+  'Made by Dex',
   'Fastest growing proxy server',
-  'Made by xBubbo',
-  'Check out discord.gg/interstellar :)',
+  'my wallet crys because of this website',
+  'V4 out now!!',
   'Thanks for using the site',
-  'Follow us on Tiktok (@useinterstellar)',
-  'Subscribe to us on YouTube (@unblocking)',
-  'Subscribe to my Youtube (@xbubbo)',
   'Check out the settings page',
-  'Check out our Patreon (https://www.patreon.com/gointerstellar)',
+  'michael chuchev',
+  'Please turn on popups',
+  'i HATE typescript',
+  'broke my laptop coding this',
+  'peanut butter oatmeal',
+  'ask me to put anything you want here',
 ]
 
 document.getElementById('splash').innerText = splashtext[Math.floor(Math.random() * splashtext.length)]
